f658b2babb928ce1fb8a61c5391a516630e79a81
Prakhar Srivastav <prakhar1989@gmail.com>
2015-10-06 20:41:25 -0400

    Sending tweets across websockets :metal:

--- twitter-experiment/server.js
+++ twitter-experiment/server.js
@@ -54,16 +54,25 @@ function getTweets(req, res, next) {
         .finally(next);
 }
 
-function startListening() {
+function startListening(socket, channel) {
     r.connect({host: config.rethinkdb.host, port: config.rethinkdb.port}, function(err, conn) {
         if (err) throw err;
-        var connection = conn;
-        r.db(config.rethinkdb.db).table(config.rethinkdb.table).changes()
+
+        // batching
+        var connection = conn, tweets = [];
+
+        r.db(config.rethinkdb.db).table(config.rethinkdb.table)
+            .pluck(['id_str', 'geo']).changes()
             .run(connection, function(err, cursor) {
                 if (err) throw err;
                 cursor.each(function(err, row) {
                     if (err) throw err;
-                    console.log(JSON.stringify(row, null, 2));
+                    tweets.push(row.new_val);
+                    if (tweets.length == 5) {
+                        socket.emit(channel, {tweets: tweets});
+                        console.log('Sending tweets:', tweets.length);
+                        tweets = [];
+                    }
                 });
             });
     });
@@ -71,10 +80,12 @@ function startListening() {
 
 io.on('connection', function(socket) {
     console.log("a user connected");
+    socket.emit('news', {hello: 'world'});
+    startListening(socket, 'tweets');
 });
 
 // start listening
 http.listen(config.express.port, function() {
     console.log('listening on 3000');
-    startListening();
+    //startListening();
 });
