078ba622f6908a88b83df87b3bd75e28ed3cdb74
Prakhar Srivastav <prakhar1989@gmail.com>
2015-10-19 09:59:19 -0400

    making it production ready

--- twittMap/config.js
+++ twittMap/config.js
@@ -1,5 +1,11 @@
 module.exports = {
     rethinkdb: {
+        host: '172.31.59.233',
+        port: 28015,
+        db: 'twitter_streaming',
+        table: 'jstwitter',
+    },
+    rethinkdb_dev: {
         host: '127.0.0.1',
         port: 28015,
         db: 'twitter_streaming',
